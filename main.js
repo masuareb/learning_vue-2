2f90d0abf6f8cc1412d99bcf5536c8cbcba06103
Arturo Suárez <masuareb@gmail.com>
2021-02-19 11:45:51 -0600

    Class and style binding

--- main.js
+++ main.js
@@ -24,6 +24,9 @@ var app = new Vue({
         addToCart() {
             this.cart += 1
         },
+        removeFromCart() {
+            this.cart -= 1
+        },
         updateProduct(variantImage) {
             this.image = variantImage
         }
